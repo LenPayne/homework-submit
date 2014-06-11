dd637d7b4b46b218bf2a9d34c73700864cf2a41d
Len Payne <len.payne@lambtoncollege.ca>
2014-06-11 11:35:47 -0400

    Added a Basic Web Server and Other Basics

--- web.js
+++ web.js
@@ -24,3 +24,39 @@ var mongo = require('mongodb');
 var bodyParser = require('body-parser');
 var scrypt = require('scrypt');
 var md5 = require('MD5');
+
+//== Setting Up the App
+var app = express();
+app.use(bodyParser());
+
+//== Pseudo-Constant Declarations
+var mongoUri = process.env.MONGOLAB_URI ||
+  process.env.MONGOHQ_URL ||
+  'mongodb://localhost/homework-submit';
+var SESSION_TIMEOUT = 600;
+
+//== Configuration for scrypt Hashing Module
+var scryptParameters = scrypt.params(0.8);
+scrypt.hash.config.keyEncoding = "ascii";
+scrypt.hash.config.outputEncoding = "hex";
+scrypt.verify.config.keyEncoding = "ascii";
+scrypt.verify.config.hashEncoding = "hex";
+
+//== Begin Section / => Static File Server ==
+//===========================================
+
+//== Default Route => Load the Homepage
+app.get('/', function(req, res) {
+  res.sendfile(path.join('public', 'index.html'));
+});
+
+//== "Web Server" Route => Translate Any File Request to Public Folder
+app.get(/^\/([\w\-\.\/]*\.(?:html|css|js|gif|png|jpeg|jpg|ico|pdf))$/, function(req, res) {
+  var filename = path.join('public', req.params[0]);
+  fs.exists(filename, function(exists) {
+    if (exists)
+      res.sendfile(filename);
+    else
+      res.status(404).sendfile(path.join('public', '404.html'));
+  })
+});
